e20038e4011a76ff7106dcb42f3a77cf9cf20c64
PhantonBlot <niko9946@edu.zealand.dk>
2023-06-01 13:57:39 +0200

    To app bar med login

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,121 +61,140 @@ function ResponsiveAppBar() {
 
   return (
     <>
-    <AppBar position="static" sx={{ backgroundColor: '#2669A4' }}>
-    <Container sx={{
+      <AppBar position="static" sx={{ backgroundColor: '#2669A4', height: '60px' }}>
+        <Container
+          sx={{
             backgroundColor: '#2669A4',
-            width:'100%',
-            height: '50px'
+            width: '100%',
+            height: '50px',
           }}>
-        <Toolbar sx={{p: 0, m: 0}}>
-        <Box sx={{ 
-          display: 'flex', 
-          color: '#ffffff', 
-          alignItems: 'center', 
-          width:'100%',
-          justifyContent: 'right'
-        }}>                    
-            <Tooltip title="Brugerinformation">
-              <IconButton onClick={handleOpenUserMenu} 
-              sx={{ 
-                p: 0,
-                m: 0,
-                borderRadius: '0.5rem'
-              }}>
-              <Typography color="#ffffff">Login</Typography>
-                <Avatar src="../icons/login.png" 
-                sx={{ bgcolor: 'transparent' }}
+          <Toolbar sx={{ p: 0, m: 0 }}>
+            <Box sx={{
+              display: 'flex',
+              color: '#ffffff',
+              alignItems: 'center',
+              width: '100%',
+              justifyContent: 'right'
+            }}>
+              <Tooltip title="Brugerinformation">
+                <IconButton onClick={handleOpenUserMenu}
+                  sx={{
+                    p: 0,
+                    m: 0,
+                    borderRadius: '0.5rem'
+                  }}>
+                  <Typography color="#ffffff">Login</Typography>
+                  <Avatar src="../icons/login.png"
+                    sx={{ bgcolor: 'transparent' }}
                   />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-    <AppBar position="static" sx={{ backgroundColor: '#FFFFFF' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <a href="/"><img className="headerLogo" src={require('../images/RBLogo.png')} width={"100px"} alt='logo'></img></a>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+      <AppBar position="static" sx={{ backgroundColor: '#FFFFFF' }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <a href="/"><img className="headerLogo" src={require('../images/RBLogo.png')} width={"100px"} alt='logo'></img></a>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
               sx={{
-                display: { xs: 'block', md: 'none' },
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
               }}
             >
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'space-evenly' } }}>
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    color: 'black',
+                    display: 'block',
+                    fontSize: 20,
+                    fontWeight: 600,
+                    textTransform: 'none'
+                  }}
+                >
+                  {page}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'space-evenly' } }}>
-            {pages.map((page) => (
+              {/* 
+            DROP DOWN MENU FOR U HOLD 
+            */}
               <Button
-                key={page}
-                onClick={handleCloseNavMenu}
+                onClick={handleClick}
+                onMouseOver={handleClick}
                 sx={{
                   my: 2,
                   color: 'black',
@@ -185,55 +204,37 @@ function ResponsiveAppBar() {
                   textTransform: 'none'
                 }}
               >
-                {page}
+                Holdoversigt
               </Button>
-            ))}
-            {/* 
-            DROP DOWN MENU FOR U HOLD 
-            */}
-            <Button
-              onClick={handleClick}
-              onMouseOver={handleClick}
-              sx={{
-                my: 2,
-                color: 'black',
-                display: 'block',
-                fontSize: 20,
-                fontWeight: 600,
-                textTransform: 'none'
-              }}
-            >
-              Holdoversigt
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              MenuListProps={{ onMouseLeave: handleClose }}
-            >
-              {uHold.map((hold) => (
-                <MyMenuItem
-                  key={hold}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    color: 'black',
-                    display: 'block',
-                    fontSize: 20,
-                    fontWeight: 600,
-                    textTransform: 'none',
-                    minWidth: '120px'
-                  }}
-                >
-                  {hold}
-                </MyMenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                MenuListProps={{ onMouseLeave: handleClose }}
+              >
+                {uHold.map((hold) => (
+                  <MyMenuItem
+                    key={hold}
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      my: 2,
+                      color: 'black',
+                      display: 'block',
+                      fontSize: 20,
+                      fontWeight: 600,
+                      textTransform: 'none',
+                      minWidth: '120px'
+                    }}
+                  >
+                    {hold}
+                  </MyMenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   );
 }
