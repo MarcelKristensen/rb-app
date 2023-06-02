6e9868180ef3eabfa92c689fec99fbeb52e2ec33
Marcel Kristensen <marcel@geoprof.dk>
2023-06-02 18:56:36 +0200

    restyled footer og sponsor + image folders

--- src/components/nyheder.js
+++ src/components/nyheder.js
@@ -1,36 +1,32 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions, Link, styled } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions, Link } from "@mui/material";
 
 export default function SenesteNyheder(props) {
   return (
     <>
-      <Card sx={{ mx: '1%', my: '5%' }}>
+      <Card sx={{ mx: "1%", my: "5%" }}>
         <CardActionArea>
           <CardContent
             sx={{
-              backgroundColor: '#ffffff', minHeight: '80px'
-            }}>
-            <h1 
-            component="div">
-              {props.title}
-            </h1>
-            
-            <CardMedia
-              component="img"
-              image={props.image}
-              alt={props.alt}
-            />
+              backgroundColor: "#ffffff",
+              minHeight: "80px",
+            }}
+          >
+            <h1 component="div">{props.title}</h1>
+
+            <CardMedia component="img" image={props.image} alt={props.alt} />
             <Typography mt="1%" fontSize={18}>
               {props.text}
             </Typography>
             <CardActions
               sx={{
-                backgroundColor: '#ffffff'
-              }}>
+                backgroundColor: "#ffffff",
+              }}
+            >
               <Link href="#" color="#000000">
                 Læs mere...
               </Link>
