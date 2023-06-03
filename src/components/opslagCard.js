0b60b067355201639297496d24a5c1ddc645f346
Marcel Kristensen <marcel@geoprof.dk>
2023-06-03 14:19:16 +0200

    lavet card om til flex column samt andre små justeringer

--- src/components/opslagCard.js
+++ src/components/opslagCard.js
@@ -1,24 +1,30 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Box } from '@mui/material';
-import { CommentSharp, ReplyAllSharp, ReplySharp, ShareSharp, ThumbUpSharp } from '@mui/icons-material';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Avatar, Box, Stack } from "@mui/material";
+import {
+  CommentSharp,
+  ReplyAllSharp,
+  ReplySharp,
+  ShareSharp,
+  ThumbUpSharp,
+} from "@mui/icons-material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
@@ -31,31 +37,48 @@ export default function RecipeReviewCard(props) {
   };
 
   return (
-    <Card sx={{ maxWidth: 280, mb:'3%' }}>
+    <Card
+      sx={{
+        maxWidth: 350,
+        mb: "3%",
+        boxShadow: 10,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <Box
         sx={{
-          display: 'flex',
-          alignItems: 'center',
-          mx: '4%'
-        }}>
-        <img src={require("../images/RBLogo.png")} alt="logo" width={40} height={40} marginLeft="10px"></img>
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+          mx: "4%",
+        }}
+      >
         <CardHeader
+          avatar={
+            <Avatar alt="RB Logo" src={require("../images/RBLogo.png")} />
+          }
           title="RB 1906"
           subheader={props.date}
-        />
-      </Box>
-      <CardContent
-        sx={{
-          p: 0,
-        }}>
-        <Typography mx="8%" mb="2%">{props.description}</Typography>
+        ></CardHeader>
         <CardMedia
           component="img"
           image={props.image}
           alt={props.alt}
+          height={180}
         />
-      </CardContent>
-      
+        <CardContent
+          sx={{
+            p: 0,
+          }}
+        >
+          <Typography fontSize={18} mb="2%">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </Box>
+
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <h3>{props.contentTitle}</h3>
--- src/pages/Forside.js
+++ src/pages/Forside.js
@@ -1,31 +1,40 @@
-import { Box, Typography, Grid, Link, styled, Button, Stack, Item } from "@mui/material";
-import HeroBanner from '../components/hero'
-import SenesteNyheder from '../components/senesteNyheder'
-import OpslagCard from '../components/opslagCard'
-import Hero from '../images/rb-hero.png'
+import {
+  Box,
+  Typography,
+  Grid,
+  Link,
+  styled,
+  Button,
+  Stack,
+  Item,
+} from "@mui/material";
+import HeroBanner from "../components/hero";
+import SenesteNyheder from "../components/senesteNyheder";
+import OpslagCard from "../components/opslagCard";
+import Hero from "../images/rb-hero.png";
 import React from "react";
-import Nyhed1 from '../images/nyhed-1.png';
-import Nyhed2 from '../images/nyhed-2.png';
-import Nyhed3 from '../images/nyhed-3.png';
-import Nyhed4 from '../images/nyhed-4.png';
-import cardOpslag1 from '../images/cardOpslag-1.png'
-import cardOpslag2 from '../images/cardOpslag-2.png'
-import cardOpslag3 from '../images/cardOpslag-3.png'
-import cardOpslag4 from '../images/cardOpslag-4.png'
-import cardOpslag5 from '../images/cardOpslag-5.png'
+import Nyhed1 from "../images/nyhed-1.png";
+import Nyhed2 from "../images/nyhed-2.png";
+import Nyhed3 from "../images/nyhed-3.png";
+import Nyhed4 from "../images/nyhed-4.png";
+import cardOpslag1 from "../images/cardOpslag-1.png";
+import cardOpslag2 from "../images/cardOpslag-2.png";
+import cardOpslag3 from "../images/cardOpslag-3.png";
+import cardOpslag4 from "../images/cardOpslag-4.png";
+import cardOpslag5 from "../images/cardOpslag-5.png";
 
 // Button er herfra: https://mui.com/system/styles/basics/#:~:text=to%20start%20editing-,Styled%20components%20API,-Note%3A%20this%20only
 const MyButton = styled(Button)({
-  background: '#2669A4',
+  background: "#2669A4",
   border: 0,
   borderRadius: 5,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
+  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  color: "white",
   height: 50,
-  padding: '0 30px',
+  padding: "0 30px",
   "&:hover": {
-    backgroundColor: '#F3B820'
-  }
+    backgroundColor: "#F3B820",
+  },
 });
 
 export default function Forside() {
@@ -34,30 +43,54 @@ export default function Forside() {
       <HeroBanner header="Roskilde Boldklub" subtitle="af 1906" image={Hero} />
 
       {/* Nyheder */}
-      <Box
-        sx={{ mx: '20%' }}>
+      <Box sx={{ mx: "20%" }}>
         <h1>Seneste nyheder</h1>
       </Box>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          mx: '20%'
-        }}>
-        <SenesteNyheder image={Nyhed1} title="Doner din gamle fodbold uanset stand" date="22. Maj 2023" alt="Nyhed 1"></SenesteNyheder>
-        <SenesteNyheder image={Nyhed2} title="Ny kunsgræs på banen er spilleklar næste uge" date="12. Maj 2023" alt="Nyhed 2"></SenesteNyheder>
-        <SenesteNyheder image={Nyhed3} title="Elitehold giver ikke op på hjemmebanen!" date="6. Maj 2023" alt="Nyhed 3"></SenesteNyheder>
-        <SenesteNyheder image={Nyhed4} title="U9 drenge vinder over Svinninge" date="5. Maj 2023" alt="Nyhed 4"></SenesteNyheder>
+          display: "flex",
+          justifyContent: "space-between",
+          mx: "20%",
+        }}
+      >
+        <SenesteNyheder
+          image={Nyhed1}
+          title="Doner din gamle fodbold uanset stand"
+          date="22. Maj 2023"
+          alt="Nyhed 1"
+        ></SenesteNyheder>
+        <SenesteNyheder
+          image={Nyhed2}
+          title="Ny kunsgræs på banen er spilleklar næste uge"
+          date="12. Maj 2023"
+          alt="Nyhed 2"
+        ></SenesteNyheder>
+        <SenesteNyheder
+          image={Nyhed3}
+          title="Elitehold giver ikke op på hjemmebanen!"
+          date="6. Maj 2023"
+          alt="Nyhed 3"
+        ></SenesteNyheder>
+        <SenesteNyheder
+          image={Nyhed4}
+          title="U9 drenge vinder over Svinninge"
+          date="5. Maj 2023"
+          alt="Nyhed 4"
+        ></SenesteNyheder>
       </Box>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        my: '2%'
-      }}>
-        <Link href="/#" style={{ textDecoration: 'none' }}>
-          <MyButton sx={{
-            textTransform: 'none'
-          }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          my: "2%",
+        }}
+      >
+        <Link href="/#" style={{ textDecoration: "none" }}>
+          <MyButton
+            sx={{
+              textTransform: "none",
+            }}
+          >
             <Typography variant="blueBtn">Alle nyheder</Typography>
           </MyButton>
         </Link>
@@ -66,14 +99,15 @@ export default function Forside() {
       {/* Social Medie */}
       <Box
         sx={{
-          mx: '20%'
-        }}>
-
+          mx: "20%",
+        }}
+      >
         <Stack
-          spacing={{ xl: 0, sm: 2 }}
-          direction="row" useFlexGap
+          spacing={{ xl: 5, sm: 4 }}
+          direction="row"
+          useFlexGap
           flexWrap="wrap"
-          justifyContent={"space-between"}
+          justifyContent="center"
         >
           <OpslagCard
             date="5 dage siden"
@@ -143,7 +177,7 @@ export default function Forside() {
           <OpslagCard
             date="1 Febuar 2023"
             description="U15 er taget et smut til Helsingør. Kampstart kl. 12.30 "
-            image={''}
+            image={""}
             contentTitle="Bananer sidder fast i automaten"
             contentText1="1. Pisk æg, sukker og vaniljesukker til en luftig æggesnaps."
             contentText2="2. land mel og bagepulver og vend det forsigtigt i dejen skiftevis med smørret. Vend de mosede bananer i."
@@ -154,60 +188,118 @@ export default function Forside() {
           />
         </Stack>
       </Box>
-      
+
       {/* Sponsor */}
-      <Box sx={{
-        backgroundColor: '#312B2B',
-        pt: '2rem',
-        justifyContent: 'space-evenly'
-      }}>
+      <Box
+        sx={{
+          backgroundColor: "#312B2B",
+          pt: "2rem",
+          justifyContent: "space-evenly",
+        }}
+      >
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            color: '#FFFFFF',
-            mx: '20%',
-            justifyContent: 'space-evenly'
-          }}>
+            display: "flex",
+            flexDirection: "row",
+            color: "#FFFFFF",
+            mx: "20%",
+            justifyContent: "space-evenly",
+          }}
+        >
           <h3>SPONSORERE OG PARTNERE</h3>
         </Box>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            color: '#FFFFFF',
-            mx: '20%',
-            mt: '3rem',
-            justifyContent: 'space-evenly'
-          }}>
-          <Grid container spacing={2} columns={12} alignItems="center" mb="6rem">
+            display: "flex",
+            flexDirection: "row",
+            color: "#FFFFFF",
+            mx: "20%",
+            mt: "3rem",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Grid
+            container
+            spacing={2}
+            columns={12}
+            alignItems="center"
+            mb="6rem"
+          >
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-nike.png')} width={"90%"} alt='Sponsor-nike'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-nike.png")}
+                  width={"90%"}
+                  alt="Sponsor-nike"
+                ></img>
+              </a>
             </Grid>
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-bauhaus.png')} width={"90%"} alt='Sponsor-bauhaus'></img></a>
-
-            </Grid><Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-fysio.png')} width={"90%"} alt='Sponsor-fysio'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-bauhaus.png")}
+                  width={"90%"}
+                  alt="Sponsor-bauhaus"
+                ></img>
+              </a>
             </Grid>
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-home.png')} width={"90%"} alt='Sponsor-home'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-fysio.png")}
+                  width={"90%"}
+                  alt="Sponsor-fysio"
+                ></img>
+              </a>
             </Grid>
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-ok.png')} width={"90%"} alt='Sponsor-ok'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-home.png")}
+                  width={"90%"}
+                  alt="Sponsor-home"
+                ></img>
+              </a>
             </Grid>
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-puma.png')} width={"90%"} alt='Sponsor-puma'></img></a>
-
-            </Grid><Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-spar-nord.png')} width={"90%"} alt='Sponsor-spar-nord'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-ok.png")}
+                  width={"90%"}
+                  alt="Sponsor-ok"
+                ></img>
+              </a>
             </Grid>
             <Grid item xs={3}>
-              <a href="/#"><img src={require('../images/sponsor-sport24.png')} width={"90%"} alt='Sponsor-home'></img></a>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-puma.png")}
+                  width={"90%"}
+                  alt="Sponsor-puma"
+                ></img>
+              </a>
+            </Grid>
+            <Grid item xs={3}>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-spar-nord.png")}
+                  width={"90%"}
+                  alt="Sponsor-spar-nord"
+                ></img>
+              </a>
+            </Grid>
+            <Grid item xs={3}>
+              <a href="/#">
+                <img
+                  src={require("../images/sponsor-sport24.png")}
+                  width={"90%"}
+                  alt="Sponsor-home"
+                ></img>
+              </a>
             </Grid>
           </Grid>
         </Box>
       </Box>
     </>
-  )
+  );
 }
